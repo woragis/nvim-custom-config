05b01ccb6f8dcc7c43ec3308c19f5418767307b5
woragis <masteringthecode.woragis+github@gmail.com>
2024-04-16 23:55:10 -0300

    Fix autosave and black for python

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -9,7 +9,7 @@ local opts = {
   sources = {
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff,
-    null_ls.builtins.diagnostics.black,
+    null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.formatting.prettier,
   },
